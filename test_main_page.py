c2c12a60ef481de9a703f6ac1dda5ceba4989116
Danya <danilashafarenko@gmail.com>
2024-02-02 01:01:22 +0300

    test: modify login page test with return

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,11 @@ from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_url()
 
 
 def test_guest_should_see_login_link(browser):
@@ -14,13 +15,6 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_login_form_url_correct(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
 
 
 def test_login_form_login_exists(browser):
